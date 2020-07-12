f9b06d353050389e322a8b30883500c5a4a250a3
Jodie Miners <jminers@gmail.com>
2020-07-12 21:03:03 +1000

    Display Fields complete

---
+++ force-app/main/default/lwc/localDevWrapper/localDevWrapper.js
@@ -0,0 +1,6 @@
+import { LightningElement,api } from 'lwc';
+
+export default class LocalDevWrapper extends LightningElement {
+    recordId = '0015B000018rhwnQAA';
+    @api flexipageRegionWidth = "SMALL";
+}
